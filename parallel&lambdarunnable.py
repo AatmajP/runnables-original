51cd9d5ed27e6b4d94dea21b6f49bb85dc0a5c27
Aatmaj Patro <aatmajpatro@gmail.com>
2026-08-31 01:17:06 +0530

    Implement RunnableParallel and RunnableLambda with prompts for Machine Learning topics

--- parallel&lambdarunnable.py
+++ parallel&lambdarunnable.py
@@ -11,4 +11,32 @@ from langchain_core.output_parsers import StrOutputParser
 
 #LambdaRunnable is a class that allows you to create a runnable from a
 #lambda function. It takes a lambda function as input and returns a runnable
-from langchain_core.runnables import ParallelRunnable, LambdaRunnable
+from langchain_core.runnables import RunnableParallel,RunnableLambda
+
+# Components
+model = ChatMistralAI(model="mistral-small-2603")
+parser = StrOutputParser()
+
+# Two different prompts
+short_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in 1-2 lines"
+)
+
+detailed_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in detail"
+)
+
+# Input
+topic = "Machine Learning"
+chain = RunnableParallel({
+    "short" :RunnableLambda(lambda x :x['short']) |short_prompt | model | parser ,
+    "detailed" :RunnableLambda(lambda x: x['detailed']) |detailed_prompt |model |parser
+})
+
+result = chain.invoke({
+    "short" : {"topic":"Machine Learning"},
+    "detailed" : {"topic":"Deep Learning"}
+})
+
+print(result['short'])
+print(result['detailed'])
