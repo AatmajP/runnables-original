c2f0c394a5f8b8236e38252f6f83415e9ebb7433
Aatmaj Patro <aatmajpatro@gmail.com>
2026-08-31 17:54:58 +0530

    Add sequence operations for code generation and explanation in RunnablePassthrough

--- passthroughrunnable.py
+++ passthroughrunnable.py
@@ -16,3 +16,31 @@ from langchain_core.runnables import RunnableParallel,RunnablePassthrough
 # Components
 model = ChatMistralAI(model="mistral-small-2603")
 parser = StrOutputParser()
+
+
+code_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a code generator"),
+    ("human", "{topic}")
+])
+
+explain_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant who explains code in simple terms"),
+    ("human", "Explain the following code in simple words:\n{code}")
+])
+
+
+# Create a sequence of operations 
+seq = code_prompt | model | parser
+
+seq2 = RunnableParallel(
+    {"code" :  RunnablePassthrough(),
+     "explanation" : explain_prompt | model | parser
+    }
+)
+
+chain = seq | seq2
+
+result = chain.invoke({"topic" : "please write a code of palindrome in python "})
+
+print(result['code'])
+print(result['explanation'])
