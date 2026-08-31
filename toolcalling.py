f45cf762a4c237406bcd77b6fda541f4340d6c97
Aatmaj Patro <aatmajpatro@gmail.com>
2026-08-31 19:18:01 +0530

    Add toolcalling.py for integrating ChatMistralAI and dotenv configuration

---
+++ toolcalling.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_mistralai import ChatMistralAI
+from langchain.tools import tool 
+from langchain_core.messages import HumanMessage
+#from rich import print 
